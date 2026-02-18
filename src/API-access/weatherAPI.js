9d1fbe536863eb79261e253df8fb3b4d0ce72caf
Miles Rigby <milesjamesrigby@gmail.com>
2026-02-18 11:57:15 +0000

    Created a simple object and constructor function to define the API call, allowing specific weather info to be requested
    This does not prevent the need to process the response data, but does reduce the amount sent over the internet, as well as allowing easy control of the API call

--- src/API-access/weatherAPI.js
+++ src/API-access/weatherAPI.js
@@ -1,6 +1,47 @@
-async function getWeatherDataFromAPI(location) {
+const APICallDesc = {
+    urlBase: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+    options: {
+        'key': ['JVP5DSV9MFBR6GBNS8Q4SJU57'],
+        'unitGroup': ['uk'],
+        'include': ['days', 'hours'],
+        'elements': ['datetime', 'precip', 'tempmax', 'tempmin', 'feelslikemax', 'humidity', 'uvindex', 'temp', 'feelslike', 'windspeed', 'cloudcover', 'visibility']
+    }
+}
+
+// Constructs an API call to Visual Crossing's weather API
+const ConstructAPICall = (location) => {
     
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?key=JVP5DSV9MFBR6GBNS8Q4SJU57');
+    // API base url for all calls
+    let call = APICallDesc.urlBase;
+
+    // Make user's location query
+    call = call.concat(location, '?');
+
+    // Construct optional fields of API call
+    for (const [option, fields] of Object.entries(APICallDesc.options)) {
+
+        // Each option needs an ampersand, and an equals sign before the options list
+        call = call.concat('&', option, '=');
+
+        // Create the list of option parameters, separated by commas
+        let optionList = '';
+        for (const item of fields) {
+            optionList = optionList.concat(item, ',');
+        }
+        optionList = optionList.slice(0, -1);
+
+        // Add option parameters to 
+        call = call.concat(optionList);
+    }
+
+    return call;
+}
+
+async function getWeatherDataFromAPI(location) {
+
+    const apiCall = ConstructAPICall(location);
+    
+    const response = await fetch(apiCall);
     const data = await response.json();
 
     return data;
